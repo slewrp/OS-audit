373b1ccae9830d6952d7ebab56a5682c1f40b8fa
Aleena <aleenamanesh06@gmail.com>
2026-03-28 22:22:37 +0530

    Enhance package installation output messages
    Updated output messages for package installation checks to be more user-friendly and consistent.

--- script2_package-inspector.sh
+++ script2_package-inspector.sh
@@ -2,17 +2,17 @@
 # Script 2: FOSS Package Inspector
 
 package="git"
-
+#checking if package is installed
 if dpkg -l | grep -q $package; then
-    echo "$package is installed."
+    echo "$package is installed!"
     git --version
 else
-    echo "$package is NOT installed."
+    echo "$package is not installed!"
 fi
-
+#philosophy note
 case $package in
-git) echo "Git: distributed version control system for collaboration" ;;
-apache2) echo "Apache: backbone of the web" ;;
-mysql) echo "MySQL: open-source database system" ;;
-firefox) echo "Firefox: browser supporting open web" ;;
+git) echo "Git: a distributed version control system for collaboration" ;;
+apache2) echo "Apache: the backbone of the web" ;;
+mysql) echo "MySQL: an open-source database system" ;;
+firefox) echo "Firefox: a browser supporting open web" ;;
 esac
