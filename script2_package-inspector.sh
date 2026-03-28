f5afe225fd72fe810539e81fbf57c4ac20691613
Aleena <aleenamanesh06@gmail.com>
2026-03-28 22:15:02 +0530

    Update script2_package-inspector.sh

--- script2_package-inspector.sh
+++ script2_package-inspector.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
 
+package="git"
+
+if dpkg -l | grep -q $package; then
+    echo "$package is installed."
+    git --version
+else
+    echo "$package is NOT installed."
+fi
+
+case $package in
+git) echo "Git: distributed version control system for collaboration" ;;
+apache2) echo "Apache: backbone of the web" ;;
+mysql) echo "MySQL: open-source database system" ;;
+firefox) echo "Firefox: browser supporting open web" ;;
+esac
